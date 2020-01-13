a01aea4df383b28e731801bbb4241063072508e3
icmpnorequest <sculyt10@gmail.com>
2020-01-13 19:57:07 +0800

    Updated commets

--- CRF_Model.py
+++ CRF_Model.py
@@ -1,6 +1,6 @@
 # coding=utf8
 """
-@author: Hangyu Xia
+@author: Hangyu Xia, Yantong Lai
 """
 
 import sklearn_crfsuite
@@ -8,15 +8,15 @@ from sklearn.externals import joblib
 
 
 class CRF(object):
-    def __init__(self, algorithm="lbfgs", p1=0.1, p2=0.1, max_iterations=100):  # 构造方法, 括号内为默认参数赋值
+    def __init__(self, algorithm="lbfgs", p1=0.1, p2=0.1, max_iterations=100):
         self.algorithm = algorithm
         self.p1 = p1
         self.p2 = p2
         self.max_iterations = max_iterations
-        self.model = None                           # 模型
+        self.model = None
 
     def initialize_model(self):
-        """模型初始化"""
+        """Initialize the model"""
         algorithm = self.algorithm
         p1 = float(self.p1)
         p2 = float(self.p2)
@@ -25,7 +25,7 @@ class CRF(object):
                                           max_iterations=max_iterations, all_possible_transitions=True)
 
     def forward(self, features):
-        """前向计算，输入特征，输出预测的标签"""
+        """Input Feature, Output the label"""
         if type(self.model == "<class 'sklearn_crfsuite.estimator.CRF'>"):
             return self.model.predict(features)
         else:
@@ -39,6 +39,6 @@ class CRF(object):
 
 
 if __name__ == "__main__":
-    crf = CRF()   # 使用默认参数
+    crf = CRF()         # Default parameters
     crf.initialize_model()
     print(type(crf.model))
